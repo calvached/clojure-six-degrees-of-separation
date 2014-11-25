7ad9ae5382d4f4a441f46401b829998f64e1eaa1
calvached <calvache.diana@gmail.com>
2014-11-25 15:06:25 -0600

    Moving Parser to own file

--- src/six_degrees_of_separation/core.clj
+++ src/six_degrees_of_separation/core.clj
@@ -1,16 +1,1 @@
 (ns six-degrees-of-separation.core)
-
-(defn sender-name [tweet]
-  (re-find #"\A\w+" tweet)
-  )
-
-(defn receiver-names [tweet]
-  (re-seq #"(?<=@)\w+" tweet)
-  )
-
-(defn parse-tweet [tweet]
-  {
-   :sender (sender-name tweet),
-   :receivers (receiver-names tweet)
-   }
-  )
--- test/six_degrees_of_separation/core_test.clj
+++ test/six_degrees_of_separation/core_test.clj
@@ -2,20 +2,8 @@
   (:use midje.sweet)
   (:use [six-degrees-of-separation.core]))
 
-(facts "Parser"
-  (fact "returns the sender name when given a string"
-    (sender-name "duncan: @emily, oh what a pity. I'll fill you in next week.") => "duncan"
-    (sender-name "diana: \"The spirit of x-mas is godzilla!\" #SpiritOfChristmas @twittertweet") => "diana"
-    )
-
-  (fact "returns the names of all the receivers in a message"
-    (receiver-names "christie: 'Every day, men and women, conversing, beholding and beholden.' /cc @alberta, @bob") => '("alberta", "bob")
-    (receiver-names "duncan: hey @farid and @bob") => '("farid", "bob")
-    (receiver-names "bob: @duncan, @christie so I see it is Emerson tonight") => '("duncan", "christie")
-    )
-
-  (fact "returns a parsed tweet"
-    (parse-tweet "bob: @duncan, @christie so I see it is Emerson tonight") =>
-        { :sender "bob", :receivers '("duncan", "christie") }
+(facts "Core"
+  (fact "runs a test"
+    (+ 2 3) => 5
     )
 )
