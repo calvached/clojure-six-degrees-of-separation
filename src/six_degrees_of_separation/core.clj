a2a60930233b0dca97f427c3b6b537c4ea729fe9
calvached <calvache.diana@gmail.com>
2014-11-25 13:55:16 -0600

    Refactoring too much logic

--- src/six_degrees_of_separation/core.clj
+++ src/six_degrees_of_separation/core.clj
@@ -1,9 +1,9 @@
 (ns six-degrees-of-separation.core)
 
 (defn sender-name [tweet]
-  (re-find (re-matcher #"\A\w+" tweet))
+  (re-find #"\A\w+" tweet)
   )
 
 (defn receiver-names [tweet]
-  (re-seq #"@\w+" tweet)
+  (re-seq #"(?<=@)\w+" tweet)
   )
--- test/six_degrees_of_separation/core_test.clj
+++ test/six_degrees_of_separation/core_test.clj
@@ -2,19 +2,22 @@
   (:use midje.sweet)
   (:use [six-degrees-of-separation.core]))
 
-(println "You should expect to see three failures below.")
-
 (facts "Parser"
   (fact "returns the sender name when given a string"
     (sender-name "duncan: @emily, oh what a pity. I'll fill you in next week.") => "duncan"
-    (sender-name "diana: \"The spirit of x-mas is godzilla!\" #SpiritOfChristmas @twittertweet") =>"diana"
+    (sender-name "diana: \"The spirit of x-mas is godzilla!\" #SpiritOfChristmas @twittertweet") => "diana"
     )
 
   (fact "returns the names of all the receivers in a message"
-    (receiver-names "christie: 'Every day, men and women, conversing, beholding and beholden.' /cc @alberta, @bob") => ["@alberta", "@bob"]
-    (receiver-names "duncan: hey @farid and @bob") => ["@farid", "@bob"]
-    (receiver-names "bob: @duncan, @christie so I see it is Emerson tonight") => ["@duncan", "@christie"]
+    (receiver-names "christie: 'Every day, men and women, conversing, beholding and beholden.' /cc @alberta, @bob") => '("alberta", "bob")
+    (receiver-names "duncan: hey @farid and @bob") => '("farid", "bob")
+    (receiver-names "bob: @duncan, @christie so I see it is Emerson tonight") => '("duncan", "christie")
     )
+
+  ;(fact ""
+  ;  () => "duncan"
+  ;  () => "diana"
+  ;  )
 )
 
 ; {
